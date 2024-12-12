41cabc381e769d64f87d5dccc6378ea73f23d6a2
mnguyen175 <maryannguyen2017@gmail.com>
2024-12-12 21:26:37 +0000

    my name is

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-  // write code here
+  int num = 5;
+  num = 10;
+
+  std::string stu_name = "John";
+  std::string number = "5";
+
+  std::cout << "My name is" << stu_name <<std::endl;
   
   return 0;
 }
