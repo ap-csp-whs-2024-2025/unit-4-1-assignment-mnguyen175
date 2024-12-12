5e5633e8acf3c88e3f192a5893bbf819f58434c2
mnguyen175 <maryannguyen2017@gmail.com>
2024-12-12 21:48:14 +0000

    exercise 1 & 2, work on 3

--- classwork.cpp
+++ classwork.cpp
@@ -3,13 +3,45 @@
 
 int main()
 {
-  int num = 5;
-  num = 10;
+  // Exercise 1 
+  int x;
+  int y;
+  int add_xy;
+  int sub_xy;
+  int mult_xy;
+  int div_xy;
 
-  std::string stu_name = "John";
-  std::string number = "5";
+  x = 25;
+  y = 3;
+  add_xy = x + y;
+  sub_xy = x - y;
+  mult_xy = x * y;
+  div_xy = x / y;
 
-  std::cout << "My name is" << stu_name <<std::endl;
+  std::cout << add_xy << std::endl
+            << sub_xy << std::endl
+            << mult_xy << std::endl
+            << div_xy << std::endl;
   
-  return 0;
+  // Exercise 2
+  int isSunny;
+  int hasUmbrella;
+
+  isSunny = true;
+  hasUmbrella = false;
+
+  std::cout << (isSunny && hasUmbrella) << std::endl;
+  std::cout << (isSunny || hasUmbrella) << std::endl;
+  std::cout << (!isSunny) << std::endl;
+  std::cout << (!isSunny && !hasUmbrella) << std::endl;
+
+  // Exercise 3 
+  int n;
+  double m;
+  bool hasLicense;
+
+  n = 7;
+  m = 4.0;
+  hasLicense = false;
+
 }
